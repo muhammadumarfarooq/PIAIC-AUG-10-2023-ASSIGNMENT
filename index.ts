fb44f14146e9b7ca31bcde8b7facc877ab9eedf3
Muhammad Umar Farooq <develop.umar@gmail.com>
2023-09-02 16:12:25 +0500

    cart functionality

--- index.ts
+++ index.ts
@@ -6,6 +6,33 @@ function updateArray (items: string[], indexOfNewItem: number, newItemName: stri
    return itemCopy;
 }
 
-const result= updateArray(['Mange', 'Orange', 'Banana'], 1, 'Grapes');
-console.log(result);
+// const result= updateArray(['Mange', 'Orange', 'Banana'], 1, 'Grapes');
+// console.log(result);
 
+
+// Problem # 2 (Cart Functionality)
+
+interface Item {id: string, name: string, price: number, qty: number}
+
+const cart: Item[] = [];
+
+function addItem (item: Item) {
+    cart.push(item);
+}
+
+function removeItem (itemIndex: number) {
+    cart.splice(itemIndex, 1);
+}
+
+function updateQty (itemIndex: number, newQty: number) {
+    const updatedQtyItem = {...cart[itemIndex], qty: newQty};
+    cart.splice(itemIndex, 1, updatedQtyItem);
+}
+
+addItem({id: '1', name: 'Bag', price: 2, qty: 2});
+addItem({id: '2', name: 'Lotion', price: 5, qty: 1});
+console.log("Item added", cart);
+removeItem(0);
+console.log("Remove Item", cart);
+updateQty(0, 5);
+console.log("Update Item", cart);
