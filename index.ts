6e91611b09abeea499060fdd79f4780cfc1edd17
Muhammad Umar Farooq <develop.umar@gmail.com>
2023-09-02 15:46:32 +0500

    modify array

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world");
+// Problem # 1 (Modify Array)
+
+function updateArray (items: string[], indexOfNewItem: number, newItemName: string) {
+   const itemCopy = [...items];
+   itemCopy.splice(indexOfNewItem, 0, newItemName);
+   return itemCopy;
+}
+
+const result= updateArray(['Mange', 'Orange', 'Banana'], 1, 'Grapes');
+console.log(result);
+
